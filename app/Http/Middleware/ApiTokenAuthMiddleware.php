d68555d380c1df1dc92d3d049b6b168933a4fa62
Paul Maxwell <43032031+paulmaxwell@users.noreply.github.com>
2024-04-13 18:20:38 +0100

    feat(middleware): add api token auth middleware

---
+++ app/Http/Middleware/ApiTokenAuthMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiTokenAuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $token = $request->header('Authorization');
+        if (!$token) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = User::where('api_token', hash('sha256', $token))->first();
+
+        if (!$user) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return $next($request);
+    }
+}
